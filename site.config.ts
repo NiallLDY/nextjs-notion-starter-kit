9e4321e28cbdb39e9fba99b07fe0d369107205be
NiallLDY <ildyldy@outlook.com>
2022-07-08 13:00:52 +0800

    Change to my PageID

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '0c791a7e55b4400994e1bff88e9c91f6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'FlySwiftAI',
+  domain: 'flyswiftai.com',
+  author: 'Niall Lv',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
